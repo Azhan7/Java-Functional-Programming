59bc7b993bcbbe25bbcfb1b0606a83fe104d9a89
Azhan7 <azhanali777@gmail.com>
2022-11-01 10:06:48 +0500

    add comment for better understanding

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -7,7 +7,7 @@ public class _Function {
         System.out.println(incrementByOne(0));
         System.out.println(incrementByOneFunction.apply(0));
     }
-
+    //The first Integer refers to the argument while the second Integer refers to the return type.
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
     //Imperative Approach
     static int incrementByOne(int number){
